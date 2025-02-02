2bb9d02d7a965f6c93e6a6ac5a4c56222c39cd7e
Calvin96C <calvinfueller92@gmail.com>
2025-02-02 10:00:02 +0100

    Copy content from one file to another. Start at the word "Lorem"

--- src/Main.java
+++ src/Main.java
@@ -28,5 +28,43 @@ public class Main
          */
         //endregion
 
+        //region Copy content from one file to another. Start at the word "Lorem"
+        Path targetPath = Paths.get("Resources","Output_Aufgabe_1.txt");
+        Path sourcePath = Paths.get(bausteinPath.toString(),"Download\\20_InputOutputStreams\\src\\aufgaben\\output\\Aufgabe_1.txt");
+
+        try(BufferedReader reader = Files.newBufferedReader(sourcePath);
+            BufferedWriter writer = Files.newBufferedWriter(targetPath))
+        {
+            String line = reader.readLine(); // Set the reader file to its first line and write that into the line String
+            String nextLine;
+            boolean copy = false;
+
+            // Looping through the reader file
+            while(line  != null)
+            {
+                nextLine = reader.readLine(); // Writing the next line already into this variable, so we can check if we need to set a new line later.
+
+                if (!copy && line.contains("Lorem")) // Scanning for the keyword, so we can start copying from here
+                {
+                    copy = true;
+                }
+
+                if (copy)
+                {
+                    writer.write(line);
+                    if((nextLine != null)) // If we have a next line in the reader document, we need to create a new line
+                    {
+                        writer.newLine();
+                    }
+                }
+
+                line = nextLine; // update the current line to the next line
+            }
+        }
+        catch (IOException ex)
+        {
+            ex.printStackTrace();
+        }
+        //endregion
     }
 }
